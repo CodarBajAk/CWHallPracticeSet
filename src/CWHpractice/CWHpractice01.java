d46f3678038d87f42eb4c943747f21fca6ef7dc2
CodarBajAk <codarbajak@gmail.com>
2023-02-02 18:32:19 +0530

    Practice 01 complete .  Date 02/02/023 this is first commit form Arbaj alam

---
+++ src/CWHpractice/CWHpractice01.java
@@ -0,0 +1,65 @@
+package CWHpractice;
+import java.util.Scanner;
+public class CWHpractice01 {
+    public static void main(String[] args) {
+        // Quick Quarry form ch 01 ;
+        // Question no 01
+
+        // write a program to sum three numbers in java
+//
+//        int a = 23;
+//        int b = 22;
+//        int c = 189;
+//        int d = a + b + c;
+//        System.out.println(d );
+
+//        Question no 02 ;
+
+        // Write a program to calculate CGPA Using marks of three subjects (out of 100 )
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println( " Enter your MATH Number ");
+//        float math = sc.nextFloat();
+//        System.out.println(" Enter your English number ");
+//        float eng = sc.nextFloat();
+//        System.out.println(" Enter your Physics number ");
+//        float phy = sc.nextFloat();
+//        float total = (math + eng+ phy )/ 30;
+//        if ( total > 8.0){
+//            System.out.println("very good keep learning fast ");
+//        }
+//        else {
+//            System.out.println(" focus on the studying bro ");
+//        }
+//        System.out.println("  Your Total CGPA  is "+ total);
+
+
+        // Question no 03 ;
+
+        // Write the java program which asks the user to enter his/her name  and greets them with "Hello<name> , have a good day " text.
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" Enter your name here ");
+//        String name = sc.nextLine();
+//        System.out.println( " Hello  \n  "  + name + " hava a good day bro " );
+
+        // Question no 04
+
+        //  Write a java program to convert kilometers to miles
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" Enter the kilometers which you want to convert into miles ");
+//        float kilometers = sc.nextFloat();
+//        double miles = kilometers * 0.62137119;
+//        System.out.println( " Total miles of the given kilometers is =  "+ miles);
+
+         // Question no 05 ;
+
+        // Write a java program to detect whether a number entered by the user is integer or not .
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println( " Enter a number here ");
+//
+//        System.out.println( sc.hasNextInt() );
+
+
+    }
+}
