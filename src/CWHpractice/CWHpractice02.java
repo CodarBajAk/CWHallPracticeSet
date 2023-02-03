a051034f1e253fe42f77599f830e6eb2cad906d6
CodarBajAk <codarbajak@gmail.com>
2023-02-03 21:02:58 +0530

    Practice 02 complete .  Date 03/02/023  This is Second commit form Arbaj alam

---
+++ src/CWHpractice/CWHpractice02.java
@@ -0,0 +1,48 @@
+package CWHpractice;
+import java.util.Scanner;
+public class CWHpractice02 {
+    public static void main(String[] args) {
+        // Question no 01
+        // What will be the result of the following expression Float a = 7/4*9/2
+//        System.out.println( " the sum of the total number is here bro !!!! ");
+//        float a = (7 / 4.0f) * (9 / 2.0f);
+//        System.out.println(a);
+
+
+        // Question no 02
+        // Write a java program to encrypt a grade by adding 8 to it and Decrypt it to show the correct grade to the friend ...
+
+//        System.out.println(" This is my grade which I want to  show you here with code bro !!!! " );
+//        // Here grade ko increment kiya ja raha hai
+//        char grade = 'A';
+//             grade = (char) (grade + 8) ;
+//        System.out.println(grade);
+//
+//        // Here grade ko decrement kiya ja raha hai ok
+//
+//        grade = (char) (grade - 8) ;
+//        System.out.println(grade) ;
+
+
+        // Question no 03
+        // use comparison operators to find out whether a given number is greater than the user entered number or not .
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println( " Enter any number which you want to compare to this computer ok BY ... ");
+//        int num = sc.nextInt();
+//        System.out.println(num > 23);
+
+        // Question no 04
+        // Write the following expression in a java program    v^2-u^2 / 2as
+
+        // QUESTION NO 04  i  UNABLE TO WRITE THE PROGRAM DUR TO ERRORS OF THE CHARTER OF THE GIVEN THING OK BYY
+
+
+          // Question no 05
+          // find the value of the following expression :
+//        int x = 7 ;
+//        int a = 7*49/7 + 35 / 7;
+//        System.out.println( a);
+
+    }
+}
